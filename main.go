5195387bb558c83920d9689b221949e83974a844
Yerzhan <yerzhan.raiin@gmail.com>
2025-06-26 23:05:09 +0500

    added base implementation of bot: ping-pong

--- main.go
+++ main.go
@@ -1,1 +1,63 @@
-package dota_discord_go
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	token := os.Getenv("DISCORD_TOKEN")
+	if token == "" {
+		fmt.Println("DISCORD_TOKEN environment variable not set")
+		return
+	}
+
+	session, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	session.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentsMessageContent
+
+	session.AddHandler(messageCreate)
+
+	if err = session.Open(); err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	_ = session.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	fmt.Printf("New message: %s from %s\n", m.Content, m.Author.Username)
+
+	if m.Content == "ping!" {
+		_, err := s.ChannelMessageSend(m.ChannelID, "pong!")
+		if err != nil {
+			fmt.Println("error sending message,", err)
+		}
+	}
+
+}
